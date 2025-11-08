312540bde0502b2e7a65bbc99d8280543d28406d
Jagaban <yunusamuhammad02019@gmail.com>
2025-11-08 11:13:20 +0100

    work on the responsiveness

--- Rim/src/components/userFrontEnd/product-details.jsx
+++ Rim/src/components/userFrontEnd/product-details.jsx
@@ -8,14 +8,15 @@ import { Input } from "../ui/input";
 function ProductDetailsDialog({ open, setOpen, productDetails }) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw] bg-white text-neutral-900 rounded-2xl shadow-xl">
+      <DialogContent className="grid grid-cols-2 gap-8 sm:p-8 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw] bg-white text-neutral-900 rounded-2xl shadow-lg">
         <div className="relative overflow-hidden rounded-lg">
           <img
             src={productDetails?.image}
             alt={productDetails?.title}
             width={600}
             height={600}
-            className="aspect-square w-full object-cover"
+            loading="lazy"
+            className="aspect-square w-full object-cover transform-gpu"
           />
         </div>
 
